b5f9f5c6c812a24475c4537a1fffb6c2c63778cf
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-04-07 10:03:55 -0700

    Connected to API, data response confirmed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import { useEffect } from 'react';
 import './App.css';
 
 export default function App() {
+
+  useEffect(() => {
+    fetch(`http://api.weatherapi.com/v1/current.json?key=${process.env.REACT_APP_API_KEY}&q=Sacramento`)
+      .then(res => res.json())
+      .then(data => console.log(data));
+  }, [])
+
   return (
-    <div> </div>
+    <div></div>
   );
 }
 
