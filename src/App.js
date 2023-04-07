828dfc4102c975b2ca51bc9a384f098a20ae7f3c
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-04-07 10:59:36 -0700

    Applied user search functionality

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
-import { useEffect } from 'react';
+import { useState } from 'react';
 import './App.css';
 
 export default function App() {
 
-  useEffect(() => {
-    fetch(`http://api.weatherapi.com/v1/current.json?key=${process.env.REACT_APP_API_KEY}&q=Sacramento`)
-      .then(res => res.json())
-      .then(data => console.log(data));
-  }, [])
+  // State holds location to search
+  const [location, setLocation] = useState('');
+
+  // Updates location state
+  function updateLocation(e){
+    setLocation(e.target.value);
+  }
+
+  // Performs weather search based on location
+  function performSearch() {
+    if (location !== '') {
+      fetch(`http://api.weatherapi.com/v1/current.json?key=${process.env.REACT_APP_API_KEY}&q=${location}`)
+        .then(res => res.json())
+        .then(data => console.log(data));
+    }
+  }
 
   return (
-    <div></div>
+    <div>
+      <input type="text" className='main-input' value={location} onChange={(e) => updateLocation(e)}/>
+      <button onClick={(e) => performSearch(e)}>Click</button>
+    </div>
   );
 }
 
