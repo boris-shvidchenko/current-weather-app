de60f4f341d1b4da9e63dd230d224a7f246a26ff
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-04-18 10:10:48 -0700

    Error UI added

--- src/components/Search.js
+++ src/components/Search.js
@@ -39,13 +39,17 @@ export default function Search() {
     }
 
     return (
-        <main className={`${!state.showResults && 'mt-48 sm:mt-80 w-auto sm:w-[500px] mx-4 sm:mx-auto space-y-3'} ${state.showResults && 'flex items-center justify-between space-x-5 my-4'} duration-300 ease-in-out px-10`}>
-            <h1 className='text-3xl flex-1 sm:flex-none relative left-2 font-semibold text-blue-800'>Weather Now</h1>
-            <form onSubmit={(e) => performSearch(e)} className='flex items-center space-x-2 border border-slate-600 py-2 px-4 rounded-3xl shadow-md drop-shadow-sm'>
-                <input type="text" className='w-full p-1 flex-1 outline-none' value={state.location} onChange={(e) => updateLocation(e)}/>
-                <MagnifyingGlassIcon className='w-6 h-6 cursor-pointer hover:text-blue-800' onClick={(e) => performSearch(e)} />
-            </form>
-            <p className={`${state.showResults && 'hidden'} text-sm relative realtive left-2 pt-2 text-slate-500`}>Current weather, anywhere in the world.</p>
-        </main>
+        <>
+            <main className={`${!state.showResults && 'mt-48 sm:mt-80 w-auto sm:w-[500px] mx-4 sm:mx-auto space-y-3'} ${state.showResults && 'flex items-center justify-between space-x-5 my-4'} duration-300 ease-in-out px-10`}>
+                <h1 className='text-3xl flex-1 sm:flex-none relative left-2 font-semibold text-blue-800'>Weather Now</h1>
+                <form onSubmit={(e) => performSearch(e)} className='flex items-center space-x-2 border border-slate-600 py-2 px-4 rounded-3xl shadow-md drop-shadow-sm'>
+                    <input type="text" className='w-full p-1 flex-1 outline-none' value={state.location} onChange={(e) => updateLocation(e)}/>
+                    <MagnifyingGlassIcon className='w-6 h-6 cursor-pointer hover:text-blue-800' onClick={(e) => performSearch(e)} />
+                </form>
+                <p className={`${state.showResults && 'hidden'} text-sm relative left-2 pt-2 text-slate-500`}>Current weather, anywhere in the world.</p>
+                <p className={`${!state.error && 'hidden'} ${state.showResults && 'hidden'} text-sm relative left-2 text-red-400`}>Location not found, please try again.</p>
+            </main>
+            <p className={`${(state.error && state.showResults) && 'text-sm absolute right-14 text-red-400 ml-12'} ${(!state.error || !state.showResults) && 'hidden'}`}>Location not found, please try again.</p>
+        </> 
     )
 }
